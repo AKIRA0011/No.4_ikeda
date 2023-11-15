a556bc4502205679ab86e997f5080409d114e43b
AKIRA0011 <akira03415@icloud.com>
2023-11-16 01:39:09 +0900

    [Add] modal

---
+++ modal.js
@@ -0,0 +1,19 @@
+window.onload = function () {
+  let modal = document.getElementById("addModal");
+  let btn = document.getElementById("addButton");
+  let span = document.getElementsByClassName("close")[0];
+
+  btn.onclick = function () {
+    modal.style.display = "block";
+  };
+
+  span.onclick = function () {
+    modal.style.display = "none";
+  };
+
+  window.onclick = function (event) {
+    if (event.target == modal) {
+      modal.style.display = "none";
+    }
+  };
+};
