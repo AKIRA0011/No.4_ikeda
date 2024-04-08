1b58eae90012ddd810f6f8b99e9300661cc3c4cc
a.ikeda <a.ikeda@no4.co.jp>
2024-04-08 15:35:38 +0900

    インデントを2行に変更

--- delete_page.php
+++ delete_page.php
@@ -11,23 +11,23 @@ $id = $_GET['id'];
 <html lang="ja">
 
 <head>
-    <meta charset="UTF-8">
-    <title>最終確認</title>
-    <link rel="stylesheet" href="style.css">
+  <meta charset="UTF-8">
+  <title>最終確認</title>
+  <link rel="stylesheet" href="style.css">
 </head>
 
 <body>
-    <div>
-        <h1 class="caution">最終確認!!</h1>
-    </div>
-    <div>
-        <h2>本当に削除していいですか？</h2>
-    </div>
-    <form method="post" action="delete.php">
-        <input type="hidden" name="id" value="<?php echo $id; ?>">
-        <button type="submit" class="push">はい</button>
-        <a href="todo_list_page.php" class="back">いいえ</a>
-    </form>
+  <div>
+    <h1 class="caution">最終確認!!</h1>
+  </div>
+  <div>
+    <h2>本当に削除していいですか？</h2>
+  </div>
+  <form method="post" action="delete.php">
+    <input type="hidden" name="id" value="<?php echo $id; ?>">
+    <button type="submit" class="push">はい</button>
+    <a href="todo_list_page.php" class="back">いいえ</a>
+  </form>
 </body>
 
 </html>
--- todo_list_page.php
+++ todo_list_page.php
@@ -1,9 +1,4 @@
 <?php
-session_start();
-
-//エラーメッセージのリセット
-$flash = isset($_SESSION['flash']) ? $_SESSION['flash'] : [];
-unset($_SESSION['flash']);
 
 //データベース接続を読み込む
 require('connect.php');
@@ -11,7 +6,7 @@ $modal_display_style = "style='display:none'";
 
 //post処理
 if (isset($_POST['register'])) {
-    $modal_display_style = "style='display:block'";
+  $modal_display_style = "style='display:block'";
 }
 
 //表に出力するデータベースを取ってくるsql文の実行
@@ -21,12 +16,6 @@ $stmt = $dbh->query($sql);
 //結果の取り出し
 $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
-//エスケープ処理
-function escape($s)
-{
-    return htmlspecialchars($s, ENT_QUOTES, "UTF-8");
-}
-
 //接続終了
 $dbh = null;
 ?>
@@ -35,96 +24,78 @@ $dbh = null;
 <html lang="ja">
 
 <head>
-    <meta charset="UTF-8">
-    <title>To Do List</title>
+  <meta charset="UTF-8">
+  <title>To Do List</title>
 
-    <!--CSSの読み込み-->
-    <link rel="stylesheet" href="style.css">
+  <!--CSSの読み込み-->
+  <link rel="stylesheet" href="style.css">
 
-    <!--JavaScriptの読み込み-->
-    <script src="modal.js"></script>
+  <!--JavaScriptの読み込み-->
+  <script src="modal.js"></script>
 </head>
 
 <body>
-    <div>
-        <h1>ToDoリスト</h1>
+  <div>
+    <h1>ToDoリスト</h1>
+  </div>
+  <div id="addButton">
+    <button type="button">
+      追加
+    </button>
+  </div>
+  <table>
+    <thead class="header">
+      <tr>
+        <th id="num">番号</th>
+        <th id="title">タイトル</th>
+        <th id="content">内容</th>
+        <th id="create">作成日</th>
+        <th id="edit">更新日</th>
+        <th id="btn"></th>
+      </tr>
+    </thead>
+
+    <!--データベース表-->
+    <tbody>
+      <?php
+      foreach ($result as $row) {
+      ?>
+        <tr>
+          <td><?php echo $row['id'] ?></td>
+          <td><?php echo $row['title'] ?></td>
+          <td><?php echo $row['todo'] ?></td>
+          <td><?php echo $row['cre'] ?></td>
+          <td><?php echo $row['upd'] ?></td>
+          <td>
+            <div>
+              <form method="post">
+                <a href="edit_page.php?id=<?php echo $row['id']; ?>">編集</a>
+                <a href="delete.php?id=<?php echo $row['id']; ?>">削除</a>
+              </form>
+            </div>
+          </td>
+        </tr>
+      <?php } ?>
+    </tbody>
+  </table>
+
+  <!--追加モーダルウィンドウ-->
+  <div id="addModal" class="addmodal">
+    <div class="addmodal-content">
+
+      <!-- ×表示 -->
+      <button class="addclose">&times;</button>
+
+      <!-- 入力フォーム -->
+      <form method="post" action="add.php" class="parent">
+        <label for="title">タイトル</label><br>
+        <input type="text" id="title" class="title" name="title" placeholder="テキストを入力" required><br>
+        <label for="content">内容</label><br>
+        <textarea id="content" class="content" name="content" placeholder="テキストを入力" required></textarea><br>
+        <button type="submit">登録</button>
+      </form>
     </div>
-    <?php echo isset($flash['title']) ? $flash['title'] : null ?></br>
-    <?php echo isset($flash['content']) ? $flash['content'] : null ?>
-    <div id="addButton">
-        <button type="button">
-            追加
-        </button>
-    </div>
-    <table>
-        <thead class="header">
-            <tr>
-                <th id="num">番号</th>
-                <th id="title">タイトル</th>
-                <th id="content">内容</th>
-                <th id="create">作成日</th>
-                <th id="edit">更新日</th>
-                <th id="btn"></th>
-            </tr>
-        </thead>
-
-        <!--データベース表-->
-        <tbody>
-            <?php
-            foreach ($result as $row) {
-            ?>
-                <tr>
-                    <td><?php echo escape($row['id']) ?></td>
-                    <td><?php echo escape($row['title']) ?></td>
-                    <td><?php echo escape($row['todo']) ?></td>
-                    <td><?php echo escape($row['cre']) ?></td>
-                    <td><?php echo escape($row['upd']) ?></td>
-                    <td>
-                        <div>
-                            <form method="post">
-                                <a href="edit_page.php?id=<?php echo $row['id']; ?>">編集</a>
-                                <a href="delete_page.php?id=<?php echo $row['id']; ?>">削除</a>
-                            </form>
-                        </div>
-                    </td>
-                </tr>
-            <?php } ?>
-        </tbody>
-    </table>
-
-    <!--追加モーダルウィンドウ-->
-    <div id="addModal" class="addmodal">
-        <div class="addmodal-content">
-
-            <!-- ×表示 -->
-            <button class="addclose">&times;</button>
-
-            <!-- 入力フォーム -->
-            <form method="post" action="add.php">
-                <label for="title">タイトル</label><br>
-                <input type="text" id="title" class="title" name="title" maxlength="30" placeholder="テキストを入力(30文字以下)" required><br>
-                <label for="content">内容</label><br>
-                <textarea id="content" class="content" name="content" maxlength="65535" placeholder="テキストを入力" required></textarea><br>
-                <button type="submit" value="登録">登録</button>
-            </form>
-        </div>
-    </div>
+  </div>
 </body>
 
 </html>
-
-<!-- //編集モーダルウィンドウ
-<div id="cntModal" class="cntmodal">
-    <div class="cntmodal-content">
-        //×表示 
-        <span class="cntclose">&times;</span>
-        //入力フォーム
-        <form method="post" action="continue.php">
-            <label for="title">タイトル</label><br>
-            <input type="text" id="title" class="text" name="title" placeholder="テキストを入力" required><br>
-            <label for="text">内容</label><br>
-            <textarea id="text" name="text" placeholder="テキストを入力" required></textarea><br>
-            <button type="submit" value="登録">登録</button>
-        </form>
-    </div>   
-</div> -->
